83e99a517551e76021d755517a15421a6857c2ed
Razwan Rashidi <razwan.izmi@gmail.com>
2020-07-31 21:24:07 +0800

    Create test for seasonal products on homepage

--- test/mango_web/acceptance/homepage_test.exs
+++ test/mango_web/acceptance/homepage_test.exs
@@ -7,6 +7,29 @@ defmodule MangoWeb.HomepageTest do
   test "presence of featured products" do
     navigate_to("/")
 
-    assert page_source() =~ "Seasonal products"
+    ## GIVEN ##
+    # There are two products Apple and Tomato priced at 100 and 50 respectively
+    # With Apple being the only seasonal product
+
+    ## WHEN ##
+    # I navigate to homepage
+    navigate_to("/")
+
+    ## THEN ##
+    # I expect the page title to be "Seasonal products"
+    page_title = find_element(:css, ".page-title") |> visible_text()
+    assert page_title == "Seasonal Products"
+
+    # And I expect Apple to be in the product displayed
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name") |> visible_text()
+    product_price = find_within_element(product, :css, ".product-price") |> visible_text()
+
+    assert product_name == "Apple"
+    # And I expect its price to be displayed on the screen
+    assert product_price == "100"
+
+    # And I expect that Tomato is not present on the screen.
+    refute page_source() =~ "Tomato"
   end
 end
