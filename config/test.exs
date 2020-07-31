6bc35b76ce07d55bf7ac7a03b88375df68397463
Razwan Rashidi <razwan.izmi@gmail.com>
2020-07-31 19:16:40 +0800

    Install and configure hound

--- config/test.exs
+++ config/test.exs
@@ -15,8 +15,10 @@ config :mango, Mango.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :mango, MangoWeb.Endpoint,
-  http: [port: 4002],
-  server: false
+  http: [port: 4001],
+  server: true
+
+config :hound, driver: "phantomjs"
 
 # Print only warnings and errors during test
 config :logger, level: :warn
--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Mango.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:hound, "~> 1.1"}
     ]
   end
 
