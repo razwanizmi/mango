bf11e25d57da6bd10bfe7e8a380a889d2c93822c
Razwan Rashidi <razwan.izmi@gmail.com>
2020-07-31 21:56:41 +0800

    Add is_seasonal to product struct

--- lib/mango/catalog.ex
+++ lib/mango/catalog.ex
@@ -2,8 +2,8 @@ defmodule Mango.Catalog do
   alias Mango.Catalog.Product
 
   def list_products do
-    product1 = %Product{name: "Tomato", price: 50}
-    product2 = %Product{name: "Apple", price: 100}
+    product1 = %Product{name: "Tomato", price: 50, is_seasonal: false}
+    product2 = %Product{name: "Apple", price: 100, is_seasonal: true}
     [product1, product2]
   end
 end
