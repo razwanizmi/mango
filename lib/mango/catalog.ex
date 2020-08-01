833cdf862369384ac9ee5ab95b8422754d7ea7d3
Razwan Rashidi <razwan.izmi@gmail.com>
2020-08-01 09:04:57 +0800

    Change products to have category

--- lib/mango/catalog.ex
+++ lib/mango/catalog.ex
@@ -2,8 +2,8 @@ defmodule Mango.Catalog do
   alias Mango.Catalog.Product
 
   def list_products do
-    product1 = %Product{name: "Tomato", price: 50, is_seasonal: false}
-    product2 = %Product{name: "Apple", price: 100, is_seasonal: true}
+    product1 = %Product{name: "Tomato", price: 50, is_seasonal: false, category: "vegetables"}
+    product2 = %Product{name: "Apple", price: 100, is_seasonal: true, category: "fruits"}
     [product1, product2]
   end
 
