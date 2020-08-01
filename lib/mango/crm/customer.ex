434b33b68d3b6de82408cfab49942a7e69a4ef21
Razwan Rashidi <razwan.izmi@gmail.com>
2020-08-01 18:51:12 +0800

    Update customer changeset to hash password

--- lib/mango/crm/customer.ex
+++ lib/mango/crm/customer.ex
@@ -5,7 +5,7 @@ defmodule Mango.CRM.Customer do
   schema "customers" do
     field :email, :string
     field :name, :string
-    field :password_hash, :string
+    field :password, :string, virtual: true
     field :phone, :string
     field :residence_area, :string
 
@@ -15,8 +15,22 @@ defmodule Mango.CRM.Customer do
   @doc false
   def changeset(customer, attrs) do
     customer
-    |> cast(attrs, [:name, :email, :phone, :residence_area, :password_hash])
-    |> validate_required([:name, :email, :phone, :residence_area, :password_hash])
+    |> cast(attrs, [:name, :email, :phone, :residence_area, :password])
+    |> validate_required([:name, :email, :phone, :residence_area, :password])
+    |> validate_format(:email, ~r/@/, message: "is invalid")
+    |> validate_length(:password, min: 6, max: 100)
     |> unique_constraint(:email)
+    |> put_hashed_password()
+  end
+
+  defp put_hashed_password(changeset) do
+    case changeset.valid? do
+      true ->
+        changes = changeset.changes
+        put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(changes.password))
+
+      _ ->
+        changeset
+    end
   end
 end
