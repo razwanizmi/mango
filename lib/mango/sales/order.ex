a525c978b6986faa1bf68366361d14d43c99a23b
Razwan Rashidi <razwan.izmi@gmail.com>
2020-08-02 10:54:59 +0800

    Change order to use line items schema

--- lib/mango/sales/order.ex
+++ lib/mango/sales/order.ex
@@ -1,11 +1,12 @@
 defmodule Mango.Sales.Order do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Mango.Sales.{Order, LineItem}
 
   schema "orders" do
-    field :line_items, {:array, :map}
     field :status, :string
     field :total, :decimal
+    embeds_many :line_items, LineItem, on_replace: :delete
 
     timestamps()
   end
