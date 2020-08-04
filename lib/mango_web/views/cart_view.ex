c5ae794ae237eba09ab728d1982149e700751409
Razwan Rashidi <razwan.izmi@gmail.com>
2020-08-04 12:18:25 +0800

    Show cart link in navbar

--- lib/mango_web/views/cart_view.ex
+++ lib/mango_web/views/cart_view.ex
@@ -1,3 +1,20 @@
 defmodule MangoWeb.CartView do
   use MangoWeb, :view
+  alias Mango.Sales.Order
+
+  def cart_count(conn = %Plug.Conn{}) do
+    cart_count(conn.assigns.cart)
+  end
+
+  def cart_count(cart = %Order{}) do
+    Enum.reduce(cart.line_items, 0, fn item, acc ->
+      acc + item.quantity
+    end)
+  end
+
+  def cart_link_text(conn) do
+    raw("""
+    <i class="fa fa-shopping-cart"></i> <span class="cart-count">#{cart_count(conn)}</span>
+    """)
+  end
 end
